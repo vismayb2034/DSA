6bc9eef205e49e67d21b371d6baf38a85f6e422e
Vismay B <2k7vismay.b@gmail.com>
2026-08-28 02:39:48 +0530

    Time: 3 ms (72.60%) | Memory: 12.3 MB (94.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,17 +1,20 @@
 class Solution(object):
     def longestPalindrome(self, s):
         d = {}
-        i = 0
 
         for ch in s:
             d[ch] = d.get(ch, 0) + 1
 
+        i = 0
+        odd = False
+
         for ch in d:
             i += (d[ch] // 2) * 2
 
-        for ch in d:
             if d[ch] % 2 == 1:
-                i += 1
-                break
+                odd = True
+
+        if odd:
+            i += 1
 
         return i
