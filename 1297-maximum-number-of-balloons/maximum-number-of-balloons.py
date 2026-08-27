6c084c62949499c50c19ed76c283ace454feffb9
Vismay B <2k7vismay.b@gmail.com>
2026-08-28 01:06:09 +0530

    Time: 11 ms (17.13%) | Memory: 12.4 MB (84.12%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -2,16 +2,18 @@ class Solution(object):
     def maxNumberOfBalloons(self, text):
         s = {'b':1, 'a':1, 'l':2, 'o':2, 'n':1}
         d = {}
-        mini = float('inf')
 
         for ch in text:
             d[ch] = d.get(ch, 0) + 1
 
-        for ch in s:
-            if d.get(ch, 0) < s[ch]:
-                return 0
+        i = 0
 
-            cur = d[ch] // s[ch]
-            mini = min(cur, mini)
+        while True:
+            for ch in s:
+                if d.get(ch, 0) - s[ch] < 0:
+                    return i
 
-        return mini
+            for ch in s:
+                d[ch] -= s[ch]
+
+            i += 1
